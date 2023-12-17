26444ff2fb8d9dc0f7745f57c0e4c0805bb10919
Dev-29 <devabhiram@gmail.com>
2023-12-17 19:36:42 +0530

    Initial commit

---
+++ cache/cache.go
@@ -0,0 +1,14 @@
+package cache
+
+import "sync"
+
+type Cache struct {
+	lock sync.RWMutex
+	data map[string][]byte
+}
+
+func NewCache() *Cache {
+	return &Cache{
+		data: make(map[string][]byte),
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Dev-29/dgcache
+
+go 1.21.5
